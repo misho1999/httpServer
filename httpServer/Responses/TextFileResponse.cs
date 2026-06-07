72f9757aa0b5e1608650208f937321b6770dd4af
Mihail Nalbantov <misho_19999@abv.bg>
2026-06-07 21:17:21 +0100

    Download Content has been added.

---
+++ httpServer/Responses/TextFileResponse.cs
@@ -0,0 +1,34 @@
+﻿using httpServer.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace httpServer.Responses
+{
+    public class TextFileResponse : Response
+    {
+        public string FileName { get; init; }
+
+        public TextFileResponse(string fileName) : base(StatusCode.OK)
+        {
+            this.FileName = fileName;
+
+            this.Headers.Add(Header.ContentType, ContentType.Plain);
+        }
+
+        public override string ToString()
+        {
+            if (File.Exists(this.FileName))
+            {
+                this.Body = File.ReadAllTextAsync(this.FileName).Result;
+            }
+
+            var fileBytesCount = new FileInfo(this.FileName).Length;
+            this.Headers.Add(Header.ContentLength, fileBytesCount.ToString());
+
+            this.Headers.Add(Header.ContentDisposition, $"attachment; filename=\"{this.FileName}\"");
+
+            return base.ToString();
+        }
+    }
+}
--- httpServer/StartUp.cs
+++ httpServer/StartUp.cs
@@ -5,13 +5,49 @@ using httpServer.Responses;
 public class Startup 
 {
     public static async Task Main()
-        => await new HttpServer(routes => routes
+    {
+        await DownloadSitesAsTextFile(Startup.FileName, new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
+
+        var server = new HttpServer(routes => routes
         .MapGet("/", new TextResponse("Hello from the server!"))
         .MapGet("/Redirect", new RedirectResponse("/HTML"))
         .MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
-        .MapPost("/HTML", new TextResponse("Form submitted!", Startup.AddFormDataAction)))
-        .Start();
+        .MapPost("/HTML", new TextResponse("Form submitted!", Startup.AddFormDataAction))
+        .MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
+        .MapPost("/Content", new TextFileResponse(Startup.FileName)));
 
+        await server.Start();
+    }
+
+    private static async Task<string> DownloadWebSiteContent(string url)
+    {
+
+        var httpClient = new HttpClient();
+
+        using (httpClient)
+        {
+
+            var response = await httpClient.GetAsync(url);
+
+            var html = await response.Content.ReadAsStringAsync();
+            return html.Substring(0, 2000);
+        }
+    }
+
+    private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
+    {
+        var downloads = new List<Task<string>>();
+
+        foreach (var url in urls) 
+        { 
+            downloads.Add(DownloadWebSiteContent(url));
+        }
+        var responses = await Task.WhenAll(downloads);
+
+        var responsesString = string.Join(Environment.NewLine + new string('-', 100), responses);
+
+        await File.WriteAllTextAsync(fileName, responsesString);
+    }
     private static void AddFormDataAction(Request request, Response response)
     {
         response.Body = "";
@@ -38,5 +74,21 @@ public class Startup
             </form>
           </body>
         </html>";
+
+    private const string DownloadForm =
+        @"<!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset='utf-8' />
+            <title>Download Form</title>
+          </head>
+          <body>
+            <form action='/Content' method='POST'>
+              <input type='submit' value='Download Site Content' />
+            </form>
+          </body>
+        </html>";
+
+    private const string FileName = "content.txt";
 }
 
